7e9f982af08d526a58bf6fd453c132fa6d658dd2
yash <hsay0605@gmail.com>
2021-09-18 17:49:13 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import { Search } from "./components/search-box/search-box.component.jsx";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => this.setState({ monsters: user }));
+  }
+
+  // Arrow fun used insted of normal fun coz this is inferred from parent in case of Arrow fun whic is required in our case
+  handleChange = (e) => this.setState({ searchField: e.target.value });
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1 className='cat-header'>Cats Rolodex</h1>
+        <Search
+          placeholder="Search Cats"
+          handleChange={this.handleChange}
+        ></Search>
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
